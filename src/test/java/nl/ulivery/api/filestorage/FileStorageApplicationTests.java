d6a41b9de81d7abfd324ca3a67eee605f3d80f01
Alain Lardinois <alain@ip-ict.nl>
2022-03-30 12:20:49 +0200

    spring initializr

---
+++ src/test/java/nl/ulivery/api/filestorage/FileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.ulivery.api.filestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
